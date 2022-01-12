7b5a891f41e2e9407a5d4dffd6c5d12abdb9b67f
IzmaylovI <75639514+IzmaylovI@users.noreply.github.com>
2022-01-12 14:30:18 +0300

    Update arithmetic.h

--- include/arithmetic.h
+++ include/arithmetic.h
@@ -14,22 +14,30 @@ class lexeme{
 private:
 	double value;
 	string operation;
+	string parameter;
 	bool isV;
+	bool isPa;
 public:
 	lexeme(){}
 	lexeme(double m_value);
+	//lexeme(char m_parameter);
 	lexeme(string a);
 	~lexeme(){}
 	double getValue() { return value; }
 	double getValue() const{ return value; }
 	string getOp() { return operation; }
 	string getOp()const { return operation; }
+	string getParameter() { return parameter; }
+	string getParameter() const{ return parameter; }
 	bool isValue() { return isV; }
 	bool isValue() const{ return isV; }
+	bool isParameter() { return isPa; }
+	bool isParameter() const{ return isPa; }
 	void operator=(double x);
 	void operator=(string x);
 	void operator=(lexeme x);
 	bool operator==(const lexeme& x) const;
+	friend ostream& operator<<(ostream& out, lexeme& lex);
 };
 
 
@@ -37,9 +45,12 @@ class arithmetic
 {
 private:
 	map<string, int> rang = { {"+", 2}, {"-", 2}, {"/", 3}, {"*", 3}, {"(", 1}, {")", 0}, {"un-",4} };
+	int counteer = 0;
 public:
-	double arithmetic::tokenizing_and_polish_notation(string& a);
-	int check(lexeme* array, int kol);
+	vector<lexeme> arithmetic::tokenizing(string& a);
+	int check(vector<lexeme> array, int kol);
+	lexeme* arithmetic::polish_notation(vector<lexeme> arrt);
 	double calculate(lexeme* arrt, int kol);
+	int getCounteer() { return counteer; }
 };
 #endif // ! _ARITHMETIC_H_
